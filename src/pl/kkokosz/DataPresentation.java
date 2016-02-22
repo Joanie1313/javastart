90520dd128a3af6bb9088a9cdabdd534deae8cd0
Joanna Radka <joanna.radka@gmail.com>
2016-02-22 21:25:30 +0100

    kkokosz exercises

---
+++ src/pl/kkokosz/DataPresentation.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+
+
+
+1. wzkoryzstac konstruktor mojej klasy tak, zeby nie tworzyc obiektu 
+bufferedreader przy kolejnych wywolaniach
+czyli przechowywac wartosc buffered reader w polu klasy
+ */
+package pl.kkokosz;
+
+/**
+ *
+ * @author asia
+ */
+public class DataPresentation {
+
+    public static void main(String[] args) {
+
+        System.out.println("Dzień dobry. Podaj swoje imię:");
+
+        PobierzWartosc data = new PobierzWartosc();
+        String name = data.stringValue();
+
+        System.out.println("Podaj swój wiek:");
+        int age = data.intValue();
+        System.out.println("Ile zarabiasz:");
+        Double salary = data.doubleValue();
+        
+        System.out.println (name);
+        System.out.println (age);
+        System.out.println (salary);
+    }
+
+}
---
+++ src/pl/kkokosz/PobierzWartosc.java
@@ -0,0 +1,68 @@
+/*
+ napisac klase z jedna metoda typu pobierzwartosc i to ona bedzie tworzyla 
+ bufferdreader
+ zrobi readline i obsluzy ioexception
+ dodatkowo: dodac metody ktore beda pobieraly stringa i integera, double
+ metoda pobierz - moze byc wzbogacoan o wypisanie "podaj wartosc"
+ i zobaczyc przeciazanie metod
+ */
+package pl.kkokosz;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.*;
+
+/**
+ *
+ * @author asia
+ */
+public class PobierzWartosc {
+
+    public String stringValue() {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String wpis = reader.readLine();
+            return wpis.trim();
+        } catch (IOException e) {
+            System.out.println(":(");
+        }
+        return ""; //jak tu doszedł to znaczy, ze wpadł do catcha - tylko w tym przypadku - nie zwrocil nic ten trim
+    }
+
+    public Integer intValue() { //wersja z INteger
+   // public int intValue() {          //wersja z int i domyślną wartością 0
+        boolean enter;
+        String text = stringValue();
+        enter = "".equals(text);
+        if (!enter) {
+            try {
+                return Integer.parseInt(text);
+            } catch (NumberFormatException e) {
+                System.out.println("NumberFormatException: "+e);
+            }
+        } else {
+            System.out.println("papa");
+        }
+        return null; //mogę tak zrobić, bo mam Integer klasę
+        //return 0; //jesli zostawie int i chcę zwracać domyślną wartość 
+    }
+    
+    public double doubleValue () {
+    boolean enter;
+        String text = stringValue();
+        enter = "".equals(text);
+        if (!enter) {
+            try {
+                return Double.parseDouble(text.replace(",", "."));
+            } catch (NumberFormatException e) {
+                System.out.println("NumberFormatException: "+e);
+            }
+        } else {
+            System.out.println("papa");
+        }
+        return 0;
+    }
+    
+    
+
+}
